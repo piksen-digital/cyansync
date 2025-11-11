567e4974b0a22f6157af505bdafa8220a07c8171
piksen-digital <cantrell22@hotmail.com>
2025-11-11 06:31:57 +0200

    Create mockDetectDerivative.ts

---
+++ src/utils/mockDetectDerivative.ts
@@ -0,0 +1,9 @@
+typescript name=src/utils/mockDetectDerivative.ts
+// Stub: In real flow, connect to TikTok API, etc.
+export function mockDetectDerivative(userContentUrl: string): string[] {
+  // Returns array of "matches" (stubbed)
+  return [
+    "https://tiktok.com/@otheruser/video/1234567890",
+    "https://tiktok.com/@another/video/0987654321",
+  ];
+}
