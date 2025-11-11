b74806512550acb5d35d45e2fcb17cdbcf41df8e
piksen-digital <cantrell22@hotmail.com>
2025-11-11 11:14:45 +0200

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,9 @@
-import React from "react";
-type BtnProps = {
-  children: React.ReactNode;
-  onClick?: () => void;
-  type?: "button" | "submit";
-};
-export default function Button({ children, onClick, type = "button" }: BtnProps) {
+export default function Button({ children, onClick, type = "button", className }: BtnProps) {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="bg-cyanMain text-white rounded-lg shadow-lg px-6 py-2 hover:bg-accent transition-colors font-semibold"
+      className={`bg-cyanMain text-white rounded-lg shadow-lg px-6 py-2 hover:bg-accent transition-colors font-semibold ${className || ""}`}
     >
       {children}
     </button>
